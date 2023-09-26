7af510587a2cd06e6c9815d4c735fe9215c12d19
LarissaLunguinho <llunguinho16@gmail.com>
2023-09-26 14:17:25 -0300

    Correcting details

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -4,56 +4,56 @@ export const dataFake = [
     "photo":"",
     "title":"Tanjirou Kamado",
     "description":"Um rapaz gentil que salvou sua irmã da morte quando o resto da família foi assassinada. Agora, busca vingança. Pode sentir o cheiro dos onis e dos pontos fracos de seus oponentes.",
-    "photoCover":"https://criticalhits.com.br/wp-content/uploads/2021/10/demon-slayer-1.jpg"
+    "photoCover":"https://psverso.com.br/wp-content/uploads/2022/09/tanjiro-kamado-tudo-sobre-demon-slayer-kimetsu-no-yaiba-1024x576.webp"
   },
   {
     "id":"2",
     "photo":"",
     "title":"Nezuko Kamado",
     "description":"Irmã mais nova de Tanjirou. Quando foi atacada por um oni, acabou de transformando em um também, mas diferente dos outros, ela continua protegendo seu irmão.",
-    "photoCover":"https://psverso.com.br/wp-content/uploads/2022/09/nezuko-kamado-tudo-sobre-demon-slayer-kimetsu-no-yaiba.webp"
+    "photoCover":"https://psverso.com.br/wp-content/uploads/2022/09/nezuko-transformacao-oni-habilidade-demon-slayer-kimetsu-no-yaiba.webp"
   },
   {
     "id":"3",
     "photo":"",
     "title":"Inosuke Hashibira",
     "description":"Participou com Tanjirou da Seleção Final. Usa a carcaça de uma cabeça de javali e é bastante agressivo.",
-    "photoCover":"https://www.nautiljon.com/images/perso/00/29/hashibira_inosuke_18392.webp"
+    "photoCover":"https://psverso.com.br/wp-content/uploads/2022/09/hinosuke-hashibira-habilidades-demon-slayer-kimetsu-no-yaiba.webp"
   },
   {
     "id":"4",
     "photo":"",
     "title":"Zenitsu Agatsuma",
     "description":"Participou com Tanjirou da Seleção Final. Normalmente, é um covarde, mas quando adormece, seu verdadeiro poder se revela.",
-    "photoCover":"https://criticalhits.com.br/wp-content/uploads/2022/02/Zenitsu_saves_Nezuko_from_Enmu.jpg"
+    "photoCover":"https://criticalhits.com.br/wp-content/uploads/2021/05/wallpapersden.com_zenitsu-agatsuma_1280x720.jpg"
   },
   {
     "id":"5",
     "photo":"",
     "title":"Muichirou Tokitou",
     "description":"Hashira da Névoa do Kisatsutai. É descendente dos usuários da Respiração do Sol, a técnica de respiração que deu origem a todas as outras.",
-    "photoCover":"https://cdn.oneesports.gg/cdn-data/2023/05/Anime_DemonSlayer_MuichiroTokito_MistHashira_2.jpg"
+    "photoCover":"https://sm.ign.com/ign_br/screenshot/default/muichiro-demon-slayer-tatuagem_pjzj.jpg"
   },
   {
     "id":"6",
     "photo":"",
     "title":"Mitsuri Kanroji",
     "description":"Hashira do amor. Entrou no Kisatsutai para encontrar um marido. Spoiler: Ela encontrou sim.",
-    "photoCover":"https://criticalhits.com.br/wp-content/uploads/2021/06/Mitsuri-Kanroji-1.jpg"
+    "photoCover":"https://i0.wp.com/metagalaxia.com.br/wp-content/uploads/2023/05/Mitsuri-Kanroji-2.webp?resize=1024%2C576"
   },
   {
     "id":"7",
     "photo":"",
     "title":"Gyomei Himejima",
     "description":"Hashira da Rocha do Kisatsutai. Está sempre segurando um rosário e entoando mantras budistas.",
-    "photoCover":"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgcUoajIkDlu3m8kwtj70WatwqSfjZNR7_uhHnv-r_f2_Vno1nEHcCVnJO2bH-iJlpZlKWz2hAHMWo33DnpoAzpfts_aLILlL9a_imart6XsL9NwH03sq13y4K79kMAru0O0K4RgswEFXtAvv1wDRa_CEODQIDKLBKYpiY048qzCrizNRTfsfcOh4rr/w0/Kimetsu%20no%20Yaiba%20Porque%20Gyomei%20Himejima%20Est%C3%A1%20Sempre%20Orando%20e%20Chorando.webp"
+    "photoCover":"https://pm1.aminoapps.com/7336/de49ce5fd246eda05ea756d3063c1706934ad194r1-600-414v2_hq.jpg"
   },
   {
     "id":"8",
     "photo":"",
     "title":"Giyu Tomioka",
     "description":"É um dos Hashiras do Kisatsutai, e quem guiou o Tanjirou ao Esquadrão. Desde então, se preocupa com o garoto.",
-    "photoCover":"https://static0.gamerantimages.com/wordpress/wp-content/uploads/2023/02/giyu-tomioka.jpg"
+    "photoCover":"https://criticalhits.com.br/anime/conheca-as-11-formas-da-respiracao-da-agua-de-demon-slayer/attachment/eleventh-form-dead-calm/"
   },
   {
     "id":"9",
